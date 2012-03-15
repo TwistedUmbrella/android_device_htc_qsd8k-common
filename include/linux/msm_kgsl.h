5a3f6f1092c098fedb30383f77181f7a2ce28a5a
jared waters <kushdeck@gmail.com>
2012-03-15 01:30:57 -0700

    correct qsd8k headers

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -83,11 +83,9 @@ struct kgsl_devinfo {
 	unsigned int chip_id;
 	unsigned int mmu_enabled;
 	unsigned int gmem_gpubaseaddr;
-	/*
-	* This field contains the adreno revision
-	* number 200, 205, 220, etc...
-	*/
-	unsigned int gpu_id;
+	/* if gmem_hostbaseaddr is NULL, we would know its not mapped into
+	 * mmio space */
+	unsigned int gmem_hostbaseaddr;
 	unsigned int gmem_sizebytes;
 };
 

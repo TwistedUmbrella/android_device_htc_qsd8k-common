06277af13a4939e667aeeb2463d8ee9922dc6887
jared waters <kushdeck@gmail.com>
2012-03-16 22:07:58 -0700

    use prebuilt cam for now, fix firmware, others

--- qsd8k.mk
+++ qsd8k.mk
@@ -26,7 +26,13 @@ PRODUCT_COPY_FILES += \
 
 # Firmware
 PRODUCT_COPY_FILES += \
-    device/htc/qsd8k-common/prebuilt/etc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd
+    device/htc/qsd8k-common/prebuilt/etc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/qsd8k-common/prebuilt/etc/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+    device/htc/qsd8k-common/prebuilt/etc/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin
+
+# Temp Camera.apk
+PRODUCT_COPY_FILES += \
+    device/htc/qsd8k-common/prebuilt/app/Camera.apk:system/app/Camera.apk
 
 # Permissions
 PRODUCT_COPY_FILES += \
